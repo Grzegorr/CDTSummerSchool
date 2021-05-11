5a54da1f0ea4d44ee779d9f82ea260a1f2932fa2
Grzegorr <grzegorzsochacki@outlook.com>
2021-05-11 21:40:23 +0100

    Tasks Finished

--- Matlab Code/Prepared Functions/Angle_Move.m
+++ Matlab Code/Prepared Functions/Angle_Move.m
@@ -1,4 +1,4 @@
-function [] = Angle_Move(s,angle, time_wait)
+function [last_angle] = Angle_Move(s,angle, time_wait)
 %sends a signal needed to move the servo to a required angle
 %it waits time_wait secounds before returning
 %input supported 0 - 4*360
@@ -6,12 +6,14 @@ function [] = Angle_Move(s,angle, time_wait)
 %Starting point
 us = 1500;
 
-us = us + (angle / 2);
+us = us + floor(angle*0.463);
 us_str = num2str(us);
-
 write(s,us_str,"uint8");
+
+
 log = "moved to " + us_str + "us."
 pause(time_wait)
+last_angle = angle;
 
 end
 
--- Matlab Code/Prepared Functions/SrvoControl.m
+++ Matlab Code/Prepared Functions/SrvoControl.m
@@ -2,33 +2,18 @@
 %%Full rotation is around 175 us
 %%Limit urself to 1100-1900 us to have good accuracy 
 
+clear;
+clc;
+
 serialportlist
-s = serialport("COM3",115200);
+s = serialport("COM4",115200);
 s.Terminator
 configureTerminator(s,"LF")
 
-write(s,"2000","uint8");
-pause(2);
-write(s,"1500","uint8");
-pause(2);
-write(s,"1000","uint8");
-pause(2);
-write(s,"1500","uint8");
-pause(2);
-write(s,"2000","uint8");
-pause(2);
-write(s,"1500","uint8");
-pause(2);
-write(s,"1000","uint8");
-pause(2);
-write(s,"1500","uint8");
-pause(2);
-write(s,"2000","uint8");
-pause(2);
-write(s,"1500","uint8");
-pause(2);
-write(s,"1000","uint8");
-pause(2);
+write(s,"1200","uint8");
+pause(3);
+write(s,"1800","uint8");
+pause(3);
 
 
 clear;
